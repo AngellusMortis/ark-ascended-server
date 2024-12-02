aa24ca2bd4a343d26648e72670135908a9b3f073
AngellusMortis <cbailey@mort.is>
2024-12-02 10:09:33 -0500

    Adjust removing test file

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -72,8 +72,7 @@ if ! touch "${ARK_PATH}/ShooterGame/Saved/test"; then
     exit 1
 fi
 
-# Cleanup test write
-rm "${ARK_PATH}/ShooterGame/Saved/test"
+
 
 # Update Ark Ascended
 echo "$(timestamp) INFO: Updating Ark Survival Ascended Dedicated Server"
@@ -84,6 +83,9 @@ if [ $? != 0 ]; then
     echo "$(timestamp) ERROR: steamcmd was unable to successfully initialize and update Ark Survival Ascended Dedicated Server"
     exit 1
 fi
+
+# Cleanup test write
+rm "${ARK_PATH}/ShooterGame/Saved/test"
 
 # Check that log directory exists, if not create
 if ! [ -d "${ARK_PATH}/ShooterGame/Saved/Logs/" ]; then
